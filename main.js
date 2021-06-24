62694d14b9e1668d8af77cc904e594dfd3fb673d
kikileeee <paulovpdv@gmail.com>
2021-06-24 23:43:23 +0200

    added mapping API

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(response => response.json())
-    .then(data =>{
+    .then(data => {
         let output = ''
-       data.forEach(function(user){
-           console.log(user)
-           output += `
+        data.forEach(function (user) {
+            console.log(user)
+            let prnt = Math.floor(user.price_change_percentage_24h)
+
+            let boja = ''
+            if(prnt>=0){
+                boja = 'green'
+            } else {
+                boja = 'red'
+            }
+            output += `
             <ul>
-            <div><img src='${user.image}'></img><li>  ${user.id} </li></div><div>${user.current_price}</div><b>${user.market_cap}</b>
+            <div><img src='${user.image}'></img>
+            <li>  ${user.id} </li></div>
+            <div>${user.current_price}€</div>
+            <div id='${boja}'>${prnt}%</div>
+            <b>${user.market_cap}</b>
             </ul>
            `
-           
-       })
-       document.getElementById('prostor').innerHTML += output;
+        }
+
+        )
+        document.getElementById('prostor').innerHTML += output;
+
 
     })
-    
